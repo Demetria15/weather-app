88250adf57560cf764bb28254aee7cceac9f6026
Demetria15 <73849534+Demetria15@users.noreply.github.com>
2023-09-18 10:56:56 +0200

    adding weather project files

---
+++ q4tnw3/src/index.js
@@ -0,0 +1,53 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  moscow: {
+    temp: -5,
+    humidity: 20
+  }
+};
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let day = days[now.getday()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let date = document.querySelector("#date");
+
+function showDate() {
+  let currentDate = `${day}, ${hours}:${minutes}`;
+  return currentDate;
+}
+showDate();
+
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-element");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
